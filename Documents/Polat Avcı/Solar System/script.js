c6e92e5844df92cc015d0b6f0e21b62f7eb52376
GoktugYmsk <gogtug033@outlook.com>
2023-01-21 22:11:58 +0300

    SecondGit

---
+++ Documents/Polat Avcı/Solar System/script.js
@@ -0,0 +1,13 @@
+const bir = document.getElementById('merkur')
+const iki = document.getElementById('venus')
+const uc = document.getElementById('dunya')
+const dort = document.getElementById('mars')
+const bes = document.getElementById('jupiter')
+const alti = document.getElementById('saturn')
+const yedi = document.getElementById('uranus')
+const sekiz = document.getElementById('neptun')
+const box = document.querySelector('.box')
+
+bir.addEventListener('click',function(){
+    box.innerHTML = 'Merkür'
+})
