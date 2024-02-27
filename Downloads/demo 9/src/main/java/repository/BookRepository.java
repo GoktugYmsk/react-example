99cd6be783e3769c188259643dd86c412356e30c
GoktugYmsk <gogtug033@outlook.com>
2024-02-27 11:00:55 +0300

    Merge branch 'main' of https://github.com/GoktugYmsk/React-example

---
+++ Downloads/demo 9/src/main/java/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+
+import entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
